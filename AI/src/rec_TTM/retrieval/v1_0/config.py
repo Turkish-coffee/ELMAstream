6f2c62401efdd2c151ed71e0c951997a36ddad38
Turkish-coffee <kaan2000.oflaz@gmail.com>
2024-06-09 23:41:56 +0200

    adding some variables to the config file

--- AI/src/rec_TTM/retrieval/v1_0/config.py
+++ AI/src/rec_TTM/retrieval/v1_0/config.py
@@ -1,13 +1,12 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.join(__file__, '../../../../../data/netflix_recommendations'))  
+BASE_SAVE_DIR = os.path.abspath(os.path.join(__file__, '../../../../../models/rec_TTM'))
 RATINGS_FILES = [
     os.path.join(
         BASE_DIR ,
         'combined_data_' + str(i) + '.txt'
         ) for i in range(1,5)
 ]
-
 DATA = os.path.join(BASE_DIR,'output.parquet')
-
 RETRIEVAL_EPOCHS = 3
